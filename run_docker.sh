cc09ad41be53639e261df7eb4204d87fe35ea05e
Stan <stan.verbakel@fourtress.nl>
2021-06-04 10:54:30 +0200

    overwrite file

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,5 @@
 docker build -t scrumpoker .
-docker run --name scrumpoker scrumpoker  >> result.txt
+docker run --name scrumpoker scrumpoker  > result.txt
 CONTAINER_ID=$(docker ps -aqf "name=^scrumpoker$")
 IMAGE_ID=$(docker images scrumpoker -q)
 docker container rm $CONTAINER_ID
